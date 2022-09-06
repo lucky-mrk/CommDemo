4171a4086e05268d6ae46aa3ad75dcacbf0c1b26
lucky-mrk <108858448+lucky-mrk@users.noreply.github.com>
2022-09-06 13:19:41 +0530

    Delete PropertyProject directory

--- PropertyProject/force-app/main/default/lwc/createProperties/createProperties.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import PRPOERTY_OBJECT from '@salesforce/schema/Property__c';
-export default class CreateProperty extends LightningElement {
-    objectApiName = PRPOERTY_OBJECT;
-
-}
